0678ca4070924cf1034de6a5f59dd9004085a005
Rajan Maharjan <rajanmaharjan042@gmail.com>
2023-03-06 21:42:41 +0545

    initializing heroku

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-
 from dotenv import load_dotenv
 from pathlib import Path
+import django_heroku
 
-load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
+load_dotenv()
---
+++ Procfile
@@ -0,0 +1,5 @@
+from waitress import serve
+from ExpenseTracker.wsgi import application
+
+
+web: waitress-serve --port=$PORT ExpenseTracker.wsgi:application
