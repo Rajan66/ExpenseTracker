c194816ea38e05adbb6f20e7df1d7798d52d5ebb
Rajan Maharjan <rajanmaharjan042@gmail.com>
2023-03-07 23:33:45 +0545

    adding username validation

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username error': 'Username should only contain alphanumeric characters.'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username error': 'Username not available.'}, status=409)
+        return JsonResponse({'username valid': True})
+
 
 class RegistrationView(View):
     def get(self, request):
